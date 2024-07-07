94eed5c5947ca748bf1ffd70f94387bb668b948e
David Thompson <david@thethompsons.com>
2024-07-07 16:50:07 -0400

    As of now /users is not part of the design.

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-//var usersRouter = require('./routes/users');
 
 //var viewsRouter = require('./routes/views');
 var livereload = require('livereload');
