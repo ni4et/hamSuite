7cdc3c35540c9edec777abf35ac51d1eb7e74af4
David Thompson <david@thethompsons.com>
2024-07-25 19:49:49 -0400

    cleaned up console.logs. Work in progress on responses to fetch.

--- routes/data.js
+++ routes/data.js
@@ -36,12 +36,17 @@ router.post('/profile', upload.none(), function (req, res, next) {
 
 router.post('/upload', upload.single('files'), function (req, res, next) {
   // req.body will hold the text fields, if there were any
-  console.log('before');
-  parseADIF.parseADIF(req.file.buffer, req.body, headerCallback, qsoCallback);
-  console.log('after');
 
+  let recordCount = parseADIF.parseADIF(
+    req.file.buffer,
+    req.body,
+    headerCallback,
+    qsoCallback
+  );
+
+  console.log(req.cookies);
   res.json({
-    res: 'Thanks! The file: ' + req.file.originalname + ' has been uploaded.',
+    res: `Thanks! ${recordCount} qsos from  ${req.file.originalname} imported.`,
   });
 });
 
