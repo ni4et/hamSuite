059d2eccbb7ec142c70c4ddae1e64ec2e618b843
David Thompson <david@thethompsons.com>
2024-08-24 22:26:31 -0400

    Milestone!!! First upload to database with this version.

--- routes/data.js
+++ routes/data.js
@@ -36,17 +36,21 @@ router.post('/profile', upload.none(), function (req, res, next) {
 router.post('/upload', upload.single('files'), function (req, res, next) {
   let recordCount = 777;
   // req.body will hold the text fields, if there were any
-  uploadHandler(req, res)
-    .then((count) => {
-      console.log('count= ', count);
-      recordCount = count;
-      console.log('uploadHandler.then: ');
-    })
-    .then(() => {
-      res.json({
-        res: `Thanks! ${recordCount} qsos from  ${req.file.originalname} imported.`,
+  if (req.file) {
+    uploadHandler(req, res)
+      .then((count) => {
+        console.log('count= ', count);
+        recordCount = count;
+        console.log('uploadHandler.then: ');
+      })
+      .then(() => {
+        res.json({
+          res: `Thanks! ${recordCount} qsos from  ${req.file.originalname} imported.`,
+        });
       });
-    });
+  } else {
+    res.json({ res: 'No file given' });
+  }
 });
 
 //router.post('/upload', function (req, res, next) {
@@ -163,6 +167,70 @@ async function headerCallback(hdr, options) {
   }
 }
 async function qsoCallback(qso, options) {
-  console.log('qsoCallback() ', qso);
+  // Variables to be set inside if statements:
+  let wl;
+  let lnWl;
+
+  // converts the qso times to native dates:
+  let qdate = qso.qso_date; // These are strings
+  let qtim = qso.time_on;
+  let qy = Number(qdate.substring(0, 4));
+  let qmo = Number(qdate.substring(4, 6)) - 1;
+  let qdy = Number(qdate.substring(6, 8));
+  let qh = Number(qtim.substring(0, 2));
+  let qm = Number(qtim.substring(2, 4));
+  let qs = Number(qtim.substring(4, 6));
+  let dateUTC = Date.UTC(qy, qmo, qdy, qh, qm, qs); // Needed for id
+  qso._time_on = new Date(dateUTC);
+
+  if (qso.qso_date_off) {
+    qdate = qso.qso_date_off; // These are strings
+    qtim = qso.time_off;
+    qy = Number(qdate.substring(0, 4));
+    qmo = Number(qdate.substring(4, 6)) - 1;
+    qdy = Number(qdate.substring(6, 8));
+    qh = Number(qtim.substring(0, 2));
+    qm = Number(qtim.substring(2, 4));
+    qs = Number(qtim.substring(4, 6));
+    qso._time_off = new Date(Date.UTC(qy, qmo, qdy, qh, qm, qs));
+  }
+
+  // Uses the frequency or band to set the milliseconds of the _time_on.
+  // This gives us a good probability that an id generated this way will be
+  // unique. A single operator wouldnt generate 2 qsos in the same second,
+  // a large contest operation might, but not in the same band.
+  // We can still use id to to time searches since these are usually ranges.
+  if (qso.freq) {
+    qso.freq = Number(qso.freq);
+    wl = 300 / qso.freq;
+  } else if (qso.band) {
+    let band = qso.band;
+    if (band.endsWith('mm')) {
+      wl = 0.001 * Number(band.substring(0, band.length - 2));
+    } else if (band.endsWith('cm')) {
+      wl = 0.01 * Number(band.substring(0, band.length - 2));
+    } else if (band.endsWith('m')) {
+      wl = Number(band.substring(0, band.length - 1));
+    } else throw 'No band or frequency specified!';
+  }
+  let pseudoMS = Math.log(wl) * 128;
+  // The result here is a mostly unique primary key
+  qso.id = dateUTC + Math.floor(pseudoMS);
+
+  if (options.metaId) {
+    qso._metaId = options.metaId;
+  }
+  const result = await r
+    .db(options.database)
+    .table('qso')
+    .insert(qso, {
+      durability: 'soft',
+      returnChanges: true,
+      conflict: 'update',
+    })
+    .run(conn);
+
+  console.log('qsoCallback() ', result);
+
   return Promise.resolve('qsoCallback');
 }
