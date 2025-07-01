2396c5d9dd581c24b82fae1b5dcafffa1d4c1e9f
David Thompson <david@thethompsons.com>
2025-07-01 19:33:59 -0400

    Cleaned up for debug and startup calls.

--- sockets/wsjt.js
+++ sockets/wsjt.js
@@ -1,69 +1,81 @@
-const dbg = require("../lib/dbg");
-dbgp = dbg.wsjtx;
+function wsjtInit() {
+  const { Server } = require('socket.io');
+  const io = new Server();
+  let socket = null; // Initialize socket variable
 
-dbgp.enabled = true;
-//dbgp(" debug enabled!");
+  // TODO setup a namespace for wsjt-x
 
-//const cluster = require('node:cluster');
-const dgram = require("node:dgram");
-const server = dgram.createSocket("udp4");
-const parser = require("../lib/wsjt-x-parser");
+  io.on('connection', (socket) => {
+    console.log('A client connected to wsjt-x socket server:', socket.id);
+    socket = socket; // Make socket available in the scope
 
-var clientInfo;
-// For the socket.io interface to the client
+    // Register 'on' methods for events from the client.
+    // none at this time.
 
-var socket = null; // emits to socket
-var io = null; // inbound events on io
+    socket.on('disconnect', () => {
+      console.log('Client disconnected from wsjt-x socket server:', socket.id);
+    });
+  });
+  let debugPrint = debug('wsjt');
+  debugPrint('starting wsjt-x socket server');
 
-//console.log(server);
-server.on("error", (err) => {
-  console.error(`server error\n${err.stack}`);
-  server.close();
-});
+  //console.log('wsjt-x socket server starting...');
+  //const cluster = require('node:cluster');
+  const dgram = require('node:dgram');
+  const server = dgram.createSocket('udp4');
+  const parser = require('../lib/wsjt-x-parser');
+  const dbgp = require('../lib/dbg').wsjt; // Debugging function for wsjt-x
+  var clientInfo;
 
-server.on("message", (msg, rinfo) => {
-  decodedMsg = parser.decode(msg);
-  if (decodedMsg.type == "decode") {
-    clientInfo = rinfo;
-    //console.log(decodedMsg, rinfo);
-    //console.log(`from: ${rinfo.address}:${rinfo.port}`);
-    //console.dir(decodedMsg);
-    let decode = {
-      snr: decodedMsg.snr,
-      delta_frequency: decodedMsg.delta_frequency,
-      message: decodedMsg.message,
-      type: decodedMsg.message_decode.type,
-      time: decodedMsg.time,
-      de_call: decodedMsg.de_call,
-    };
-    socket.emit("decode", decode);
+  //console.log(server);
+  server.on('error', (err) => {
+    console.error(`server error\n${err.stack}`);
+    server.close();
+  });
 
-    //console.log(decode);
-  } else if (decodedMsg.type == "status") {
-    dbgp(decodedMsg);
-    if (socket) {
-      decodedMsg.freqency = Number(decodedMsg.freqency);
-      socket.emit("status", decodedMsg);
-      dbgp(decodedMsg.time);
+  server.on('message', (msg, rinfo) => {
+    decodedMsg = parser.decode(msg);
+    if (decodedMsg.type == 'decode') {
+      clientInfo = rinfo;
+      //console.log(decodedMsg, rinfo);
+      //console.log(`from: ${rinfo.address}:${rinfo.port}`);
+      //console.dir(decodedMsg);
+      let decode = {
+        snr: decodedMsg.snr,
+        delta_frequency: decodedMsg.delta_frequency,
+        message: decodedMsg.message,
+        type: decodedMsg.message_decode.type,
+        time: decodedMsg.time,
+        de_call: decodedMsg.de_call,
+      };
+      socket.emit('decode', decode);
+
+      //console.log(decode);
+    } else if (decodedMsg.type == 'status') {
+      dbgp(decodedMsg);
+      if (socket) {
+        decodedMsg.freqency = Number(decodedMsg.freqency);
+        socket.emit('status', decodedMsg);
+        dbgp(decodedMsg.time);
+      }
     }
+  });
+
+  server.on('listening', () => {
+    const address = server.address();
+    debugPrint(`server listening ${address.address}:${address.port}`);
+  });
+
+  server.bind(2237, '0.0.0.0');
+  module.exports = registerWSJTX;
+  function registerWSJTX() {
+    dbgp('registerWSJTX');
+
+    // Register 'on' methods for events from the client.
+    // none at this time.
   }
-});
 
-server.on("listening", () => {
-  const address = server.address();
-  console.log(`server listening ${address.address}:${address.port}`);
-});
-
-server.bind(2237, "0.0.0.0");
-module.exports = registerWSJTX;
-function registerWSJTX(io_in, socket_in) {
-  dbgp("registerWSJTX");
-  socket = socket_in; // Grab for local use
-  // do your emits on socket.
-  io = io_in;
-
-  // Register 'on' methods for events from the client.
-  // none at this time.
+  setTimeout(() => {}, 1000);
 }
 
-setTimeout(() => {}, 1000);
+module.exports = wsjtInit;
