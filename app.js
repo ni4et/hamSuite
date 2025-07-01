64cf3a175d027ea79e1360eb00ebcd001a33bf3c
David Thompson <david@thethompsons.com>
2025-06-30 23:43:43 -0400

    Got rid of debug for now, needs more thought.

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const dbg = require("./lib/dbg");
-dbg.app.enabled = true;
-dbg.app("App debug enabled!"); //console.clear();
+//const dbg = require("./lib/dbg");
+//dbg.app.enabled = true;
+//dbg.app("App debug enabled!"); //console.clear();
 
 console.log(
-  "========================================================================"
+  '========================================================================'
 );
 // Model for making an async function in a non-module
 /* 
@@ -12,28 +12,28 @@ console.log(
 })();
 */
 
-require("dotenv").config();
+require('dotenv').config();
 
 // This is how items in user, system, and .env are accessed:
 //console.log(process.env.DEBUG);
 
-var createError = require("http-errors");
-var express = require("express");
-var favicon = require("serve-favicon");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+var createError = require('http-errors');
+var express = require('express');
+var favicon = require('serve-favicon');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-var indexRouter = require("./routes/index");
-var dataRouter = require("./routes/data");
+var indexRouter = require('./routes/index');
+var dataRouter = require('./routes/data');
 
-var livereload = require("livereload");
-var connectLiveReload = require("connect-livereload");
+var livereload = require('livereload');
+var connectLiveReload = require('connect-livereload');
 const liveReloadServer = livereload.createServer();
 
-const { Server } = require("socket.io");
+const { Server } = require('socket.io');
 
-stationSettings = require("./public/assets/stationSettings.json");
+stationSettings = require('./public/assets/stationSettings.json');
 const stationSettingsDefault = {};
 
 for (let keya in stationSettings) {
@@ -41,7 +41,7 @@ for (let keya in stationSettings) {
   let vala = stationSettings[keya]; // details of settings
   if (vala.list) {
     // if vala.list exists then vala[0] is the default option
-    stationSettingsDefault["stationSettings_" + keya] = vala.list[0].name;
+    stationSettingsDefault['stationSettings_' + keya] = vala.list[0].name;
   }
 }
 // stationSettingsDefault will be merged with cookies for rendering
@@ -50,9 +50,9 @@ for (let keya in stationSettings) {
 
 // https://dev.to/cassiolacerda/automatically-refresh-the-browser-on-node-express-server-changes-x1f680-1k0o
 // https://github.com/livereload/livereload-js
-liveReloadServer.server.once("connection", () => {
+liveReloadServer.server.once('connection', () => {
   setTimeout(() => {
-    liveReloadServer.refresh("/");
+    liveReloadServer.refresh('/');
   }, 100);
 });
 
@@ -67,14 +67,14 @@ app.locals.stationSettings = stationSettings;
 // Install live reload js:
 app.use(connectLiveReload());
 
-if (process.env.TRACEHTTP === "all") {
+if (process.env.TRACEHTTP === 'all') {
   // This logs almost all requests.
-  app.use(logger("dev"));
+  app.use(logger('dev'));
 }
 // This logs only response codes >=400.
-else if (process.env.TRACEHTTP === "errors") {
+else if (process.env.TRACEHTTP === 'errors') {
   app.use(
-    logger("dev", {
+    logger('dev', {
       skip: function (req, res) {
         return res.statusCode < 400;
       },
@@ -83,14 +83,14 @@ else if (process.env.TRACEHTTP === "errors") {
 }
 
 // Serve favicon https://expressjs.com/en/resources/middleware/serve-favicon.html
-app.use(favicon(path.join(__dirname, "public/images", "favicon.ico")));
+app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
 
 // view engine setup
 // EJS is preferred since C/P links all provide straight HTML.
 // Besides: VSCode does most of what pug would do in the editor.
-let ejs = require("ejs");
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "/views"));
+let ejs = require('ejs');
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -107,43 +107,43 @@ app.use(function (req, res, next) {
   next();
 });
 
-var qs = require("qs");
-app.set("query parser", function (str) {
+var qs = require('qs');
+app.set('query parser', function (str) {
   return qs.parse(str, {
     /* custom options */
   });
 });
 
 // Files in 'public' appear at the top level.
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Serves node_modules as /assets/npm, to facilite popper and bootstrap.
 // Urls for these are C/P with minimal editing.
-app.use("/assets/npm", express.static(path.join(__dirname, "node_modules")));
+app.use('/assets/npm', express.static(path.join(__dirname, 'node_modules')));
 
 // Just do a no-op get in order to sync the server's cookies with the client.
 // the event callback in the view does this as the last thing after
 // setting the cookie on the client. TODO less expensive way to do this?
 app.get(
-  "/refreshCookies",
+  '/refreshCookies',
 
   function (req, res) {
-    res.send("Thank You!");
+    res.send('Thank You!');
   }
 );
-app.use("/", indexRouter);
+app.use('/', indexRouter);
 
-app.use("/data", dataRouter); // Database lives under here!
+app.use('/data', dataRouter); // Database lives under here!
 
 /* GET file from views. */
-app.get("/\\w+", function (req, res, next) {
+app.get('/\\w+', function (req, res, next) {
   const view = req.url.substring(1);
 
   // render any ejs file in
 
   res.render(view, {
     cookies: req.cookies,
-    title: "HamSuite (app.js)",
+    title: 'HamSuite (app.js)',
     view: view,
   });
 });
@@ -157,11 +157,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
 module.exports = app;
